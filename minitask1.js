441f2416a4b489470ce7354b2df9b011c76a59b8
Itsna Maulana Hasan <86395760+ItsnaMaulanaHasan@users.noreply.github.com>
2025-09-03 11:24:54 +0700

    Add files via upload

---
+++ minitask1.js
@@ -0,0 +1,55 @@
+// buatkan variabel bertipe object, lalu mendapatkan hasil = koda, tapi yang harus dilakukan untuk mendapatkan koda, saya harus meakses 
+
+const obj = {
+    str: [
+        null,
+        null,
+        null,
+        [
+            null,
+            [
+                null,
+                null,
+                {
+                    man: [
+                        {
+                            the: {
+                                name: {
+                                    of: {
+                                        us: "Koda"
+                                    }
+                                }
+                            }
+                        }
+                    ]
+                }
+            ]
+        ]
+    ]
+}
+
+console.log(obj.str[3][1][2].man[0].the.name.of.us)
+// Koda
+
+const hello = {
+    world: "Hello World"
+}
+console.log(hello.world)
+// "Hello World"
+
+const my = [
+    {
+        favourite: [
+            null,
+            null,
+            null,
+            {
+                fruit: {
+                    is: "Apple"
+                }
+            }
+        ]
+    }
+]
+console.log(my[0].favourite[3].fruit.is)
+// "Apple"

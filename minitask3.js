b8ed333f03703c759895a8aed4e2323c363e8d20
Itsna Maulana Hasan <86395760+ItsnaMaulanaHasan@users.noreply.github.com>
2025-09-03 23:54:40 +0700

    Add files via upload

--- minitask3.js
+++ minitask3.js
@@ -1,26 +1,10 @@
-//     *   
-//    * *  
-//   * * * 
-//  * * * *
-// * * * * *
-
-// * * * * *
-// *       *
-// *       *
-// *       *
-// * * * * *
-
 console.log("Pattern Persegi")
 let tinggiPersegi = 5
 for (let i = 0; i < tinggiPersegi; i++) {
     let output = ""
     if (i === 0 || i === tinggiPersegi-1){
-        for (let j = 0; j < 2*tinggiPersegi-1; j++) {
-            if (j%2==0){
-                output += "*";
-            } else {
-                output += " "
-            }
+        for (let j = 0; j < tinggiPersegi; j++) {
+            output += "* ";
         }
         console.log(output)
     } else {
@@ -43,11 +27,33 @@ for (let i = 0; i < tinggiSegitiga; i++) {
         output += " ";
     }
     for (let k = 0; k < 2 * i + 1; k++) {
-        if (k%2==0){
+        if (k%2 === 0){
             output += "*";
         } else {
             output += " "
         }
     }
     console.log(output)
+}
+
+console.log("\nPattern Labyrinth")
+for (let i = 0; i < 5; i++) {
+    let output = ""
+    if (i % 2 === 0){
+        for (let j = 0; j < 6; j++) {
+            output += "* "
+        }
+        console.log(output)
+    } else {
+        for (let k = 0; k < 11; k++){
+            if (i === 1 && k === 8){
+                output += "*"
+            } else if (k === 10) {
+                output += "*"
+            } else {
+                output += " "
+            }
+        }
+        console.log(output)
+    }
 }

607e8de70e4213d92b7761a16a0d860b02f69aca
Itsna Maulana Hasan <86395760+ItsnaMaulanaHasan@users.noreply.github.com>
2025-09-03 21:55:05 +0700

    Add files via upload

--- minitask3.js
+++ minitask3.js
@@ -1,7 +1,8 @@
-//    *   
-//   ***  
-//  ***** 
-// *******
+//     *   
+//    * *  
+//   * * * 
+//  * * * *
+// * * * * *
 
 // * * * * *
 // *       *
@@ -9,29 +10,44 @@
 // *       *
 // * * * * *
 
-// Pattern Persegi
+console.log("Pattern Persegi")
 let tinggiPersegi = 5
 for (let i = 0; i < tinggiPersegi; i++) {
-    let bintang1 = ""
+    let output = ""
     if (i === 0 || i === tinggiPersegi-1){
-        for (let j = 0; j < tinggiPersegi; j++) {
-            if (j === tinggiPersegi-1){
-                bintang1 += "*"
+        for (let j = 0; j < 2*tinggiPersegi-1; j++) {
+            if (j%2==0){
+                output += "*";
             } else {
-                bintang1 += "* "
+                output += " "
             }
         }
-        console.log(bintang1)
+        console.log(output)
     } else {
-        for (let k = 0; k < tinggiPersegi; k++) {
-            if (k === 0){
-                bintang1 += "*"
-            } else if (k === tinggiPersegi-1){
-                bintang1 += " *"
+        for (let k = 0; k < 2*tinggiPersegi-1; k++) {
+            if (k === 0 || k === 2*tinggiPersegi-2){
+                output += "*"
             } else {
-                bintang1 += "  "
+                output += " "
             }
         }
-        console.log(bintang1)
+        console.log(output)
     }
+}
+
+console.log("\nPattern Segitiga Sama Kaki")
+let tinggiSegitiga = 5
+for (let i = 0; i < tinggiSegitiga; i++) {
+    let output = "";
+    for (let j = 0; j < tinggiSegitiga - i - 1; j++) {
+        output += " ";
+    }
+    for (let k = 0; k < 2 * i + 1; k++) {
+        if (k%2==0){
+            output += "*";
+        } else {
+            output += " "
+        }
+    }
+    console.log(output)
 }

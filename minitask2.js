962890ff11e42d04c423a8e6ea3f5a394a812219
Itsna Maulana Hasan <86395760+ItsnaMaulanaHasan@users.noreply.github.com>
2025-09-03 17:12:02 +0700

    Add files via upload

--- minitask2.js
+++ minitask2.js
@@ -1,7 +1,22 @@
-let line = ""
-for (let i=5; i > 0; i--){
+let tinggi = 5
+console.log("Segitiga Siku Siku dengan For")
+let bintang1 = ""
+for (let i=tinggi; i > 0; i--){
     for (let j=i; j >= i; j--){
-        line += "*"
-        console.log(line)
+        bintang1 += "*"
+        console.log(bintang1)
     }
 }
+
+console.log("\nSegitiga Siku Siku dengan Do While")
+let bintang2 = ""
+let i = tinggi //5
+do {
+    let j = i
+    do {
+        bintang2 += "*"
+        console.log(bintang2)
+        j--
+    } while (j>=i);
+    i--
+} while (i>0);

b7c878dde44a4309f7c6195ade6e4269a9e37d61
Itsna Maulana Hasan <86395760+ItsnaMaulanaHasan@users.noreply.github.com>
2025-09-03 15:38:45 +0700

    Add files via upload

--- minitask2.js
+++ minitask2.js
@@ -1,8 +1,7 @@
 let line = ""
 for (let i=5; i > 0; i--){
-    for (let j=i; j > i-1; j--){
+    for (let j=i; j >= i; j--){
         line += "*"
         console.log(line)
-        console.log(j)
     }
 }

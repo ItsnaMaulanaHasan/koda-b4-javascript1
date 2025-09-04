78f2d4509c84d6cc9d8db799b67b8483853a5ee4
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-09-04 09:04:03 +0700

    add remote git

---
+++ minitask1.js
@@ -0,0 +1,52 @@
+const obj = {
+    str: [
+        null,
+        null,
+        null,
+        [
+            null,
+            [
+                null,
+                null,
+                {
+                    man: [
+                        {
+                            the: {
+                                name: {
+                                    of: {
+                                        us: "Koda"
+                                    }
+                                }
+                            }
+                        }
+                    ]
+                }
+            ]
+        ]
+    ]
+}
+console.log(obj.str[3][1][2].man[0].the.name.of.us)
+// Koda
+
+const hello = {
+    world: "Hello World"
+}
+console.log(hello.world)
+// "Hello World"
+
+const my = [
+    {
+        favourite: [
+            null,
+            null,
+            null,
+            {
+                fruit: {
+                    is: "Apple"
+                }
+            }
+        ]
+    }
+]
+console.log(my[0].favourite[3].fruit.is)
+// "Apple"
